1243a7c1664c4fbb56c1db84aafa0f2f3df89f96
Nguyễn Quang Huy <huynq@isi.com.vn>
2021-09-16 21:38:53 +0700

    Create big.py

---
+++ big.py
@@ -0,0 +1,29 @@
+from mpl_toolkits.basemap import Basemap
+import numpy as np
+import matplotlib.pyplot as plt
+import netCDF4
+import seaborn 
+
+plt.figure()
+url='https://nomads.ncep.noaa.gov/dods/wave/nww3/nww320210316/nww320210316_06z'
+
+file = netCDF4.Dataset(url)
+lat  = file.variables['lat'][:]
+lon  = file.variables['lon'][:]
+data = file.variables['htsgwsfc'][1,:,:]
+file.close()
+
+m=Basemap(projection='mill',lat_ts=10,llcrnrlon=lon.min(), \
+  urcrnrlon=lon.max(),llcrnrlat=lat.min(),urcrnrlat=lat.max(), \
+  resolution='c')
+x, y = m(*np.meshgrid(lon,lat))
+
+m.pcolormesh(x,y,data,shading='auto',cmap='Blues')
+m.drawcoastlines()
+m.fillcontinents()
+m.drawmapboundary()
+m.drawparallels(np.arange(-90.,120.,30.),labels=[1,0,0,0])
+m.drawmeridians(np.arange(-180.,180.,60.),labels=[0,0,0,1])
+
+plt.title('HuyRemy just said: Very Big Data. ')
+plt.show()
