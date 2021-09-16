ba546d4fade2f2cf2387e4e552652fb48bd2db72
Nguyễn Quang Huy <huynq@isi.com.vn>
2021-09-16 22:10:46 +0700

    Update big.py

--- big.py
+++ big.py
@@ -2,7 +2,6 @@ from mpl_toolkits.basemap import Basemap
 import numpy as np
 import matplotlib.pyplot as plt
 import netCDF4
-import seaborn 
 
 plt.figure()
 url='https://nomads.ncep.noaa.gov/dods/wave/nww3/nww320210316/nww320210316_06z'
@@ -14,7 +13,7 @@ data = file.variables['htsgwsfc'][1,:,:]
 file.close()
 
 m = Basemap(width=12000000,height=9000000,projection='lcc',
-            resolution='c',lat_1=45.,lat_2=55,lat_0=50,lon_0=-107.)
+            resolution='c', lat_1=21.,lat_2=55,lat_0=50,lon_0=105.)
             
 x, y = m(*np.meshgrid(lon,lat))
 
